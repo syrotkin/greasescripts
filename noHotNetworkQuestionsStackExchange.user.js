6e68965405ba5c31453e797575fdf7439b8ca76c
Oleksiy Syrotkin <oleksiy.syrotkin@elca.ch>
2015-02-04 16:32:52 +0100

    Hide "hireme" and "adzerk" divs.

--- noHotNetworkQuestionsStackExchange.user.js
+++ noHotNetworkQuestionsStackExchange.user.js
@@ -1,10 +1,34 @@
 // ==UserScript==
-// @name        noHotNetworkQuestions
-// @namespace   elca.ch.osy
-// @description No hot network questions
-// @include     http://stackoverflow.com/*
-// @include     http://*.stackexchange.com/*
-// @version     1
-// @grant       none
+// @name stackoverflow-hide-hot-network-questions
+// @namespace http://www.elca.ch/osy
+// @description Hide Hot Network Questions in Stackoverflow.com
+// @include http://stackoverflow.com/*
+// @run-at document-end
 // ==/UserScript==
+
+// this function courtesy of:
+// http://stackoverflow.com/questions/4275071/javascript-getelementbyid-wildcard
+function getElementsStartsWithId( id ) {
+  var children = document.body.getElementsByTagName('*');
+  var elements = [], child;
+  for (var i = 0, length = children.length; i < length; i++) {
+    child = children[i];
+    if (child.id.substr(0, id.length) == id)
+      elements.push(child);
+  }
+  return elements;
+}
+
 document.getElementById("hot-network-questions").style.display = "none";
+document.getElementById("hireme").style.display = "none";
+
+var adzerkElements = getElementsStartsWithId("adzerk");
+if (adzerkElements) {
+  console.log("adzerk: " + adzerkElements.length);
+}
+else {
+  console.log("can't find adzerk");
+}
+for (var el in adzerkElements) {
+  el.style.display = "none";
+}
