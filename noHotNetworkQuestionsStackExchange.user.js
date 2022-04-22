2bb65668962f9e15ca58c8693527aa48b0322a5e
syrotkin <syrotkin@users.noreply.github.com>
2022-04-22 19:48:45 +0200

    add more matched URLs, use IIFE

--- noHotNetworkQuestionsStackExchange.user.js
+++ noHotNetworkQuestionsStackExchange.user.js
@@ -1,34 +1,48 @@
 // ==UserScript==
 // @name stackoverflow-hide-hot-network-questions
 // @namespace http://www.elca.ch/osy
+// @version      0.1
 // @description Hide Hot Network Questions in Stackoverflow.com
-// @include http://stackoverflow.com/*
+// @match https://stackoverflow.com/*
+// @match https://*.stackexchange.com/*
+// @match https://superuser.com/*
+// @author       You
+// @match        https://www.tampermonkey.net/index.php?version=4.16.6160&ext=fire&updated=true
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
 // @run-at document-end
 // ==/UserScript==
 
-// this function courtesy of:
-// http://stackoverflow.com/questions/4275071/javascript-getelementbyid-wildcard
-function getElementsStartsWithId( id ) {
-  var children = document.body.getElementsByTagName('*');
-  var elements = [], child;
-  for (var i = 0, length = children.length; i < length; i++) {
-    child = children[i];
-    if (child.id.substr(0, id.length) == id)
-      elements.push(child);
-  }
-  return elements;
-}
+(function() {
+    'use strict';
 
-document.getElementById("hot-network-questions").style.display = "none";
-document.getElementById("hireme").style.display = "none";
+    // Your code here...
+    // this function courtesy of:
+    // http://stackoverflow.com/questions/4275071/javascript-getelementbyid-wildcard
+    function getElementsStartsWithId( id ) {
+        var children = document.body.getElementsByTagName('*');
+        var elements = [], child;
+        for (var i = 0, length = children.length; i < length; i++) {
+            child = children[i];
+            if (child.id.substr(0, id.length) == id) {
+                elements.push(child);
+            }
+        }
+        return elements;
+    }
 
-var adzerkElements = getElementsStartsWithId("adzerk");
-if (adzerkElements) {
-  console.log("adzerk: " + adzerkElements.length);
-}
-else {
-  console.log("can't find adzerk");
-}
-for (var el in adzerkElements) {
-  el.style.display = "none";
-}
+    document.getElementById("hot-network-questions").style.display = "none";
+    document.getElementById("hireme").style.display = "none";
+
+    var adzerkElements = getElementsStartsWithId("adzerk");
+    if (adzerkElements) {
+        console.log("adzerk: " + adzerkElements.length);
+    }
+    else {
+        console.log("can't find adzerk");
+    }
+    for (var el in adzerkElements) {
+        el.style.display = "none";
+    }
+
+})();
